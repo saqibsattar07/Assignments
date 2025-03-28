105fafea3f9848309ff653b3b7d0c48e7b1b5fa4
Saqib Sattar <159882823+saqibsattar07@users.noreply.github.com>
2025-03-29 00:22:06 +0500

    updated

---
+++ Assignments 00 to 05/05_loops_control_flow/02_print_events.py
@@ -0,0 +1,5 @@
+def First_Even():
+  for i in range(20):
+     print(i * 2, end=" ")
+
+First_Even()
---
+++ Assignments 00 to 05/06_functions/05_get_name.py
@@ -0,0 +1,8 @@
+def get_name():
+    return "Saqib Sattar"
+def main():
+    name = get_name()
+    print(f"Hello, {name}")
+    
+if __name__ == "__main__":
+    main()
